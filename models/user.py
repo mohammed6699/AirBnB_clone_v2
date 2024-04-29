65e5cc92b3fc537b05c32b54291808b73d2bdf5a
root <mohammedahmed.refaat@gmail.com>
2024-04-29 04:00:20 -0700

    ./7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy.Column import Column, string
 
+#class User(BaseModel):
+#    """This class defines a user by various attributes"""
+#    email = ''
+#    password = ''
+#    first_name = ''
+#    last_name = ''
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class user(user):
+    __tablename__ = 'user'
+    email = Column(string(128), nullable = False)
+    password = Column(string(128), nullable = False)
+    first_name = Column(string(128), nullable = True)
+    last_name = Column(string(128), nullable = True)
