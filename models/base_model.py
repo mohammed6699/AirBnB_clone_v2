7a626bb9fd7771ffc2f8f94ecf4d2e5b407b4bcf
Adel <82724648+adelhassan1@users.noreply.github.com>
2024-04-30 01:55:50 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -55,14 +55,16 @@ class BaseModel:
         storage.new(self)
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        '''
+            Return dictionary representation of BaseModel class.
+        '''
+        cp_dct = dict(self.__dict__)
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct.pop('_sa_instance_state', None)
+
+        return (cp_dct)
 
      def delete(self):
         """
