d34f64ab31d2e7e29b52a1425a899e9f622d196d
Adel <82724648+adelhassan1@users.noreply.github.com>
2024-05-02 02:28:42 +0300

    Update user

--- models/user.py
+++ models/user.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
 from squalchemy.orm import relationship
 
-#class User(BaseModel):
-#    """This class defines a user by various attributes"""
-#    email = ''
-#    password = ''
-#    first_name = ''
-#    last_name = ''
 
-class user(Base):
+class user(BaseModel, Base):
     __tablename__ = 'user'
     email = Column(String(128), nullable = False)
     password = Column(String(128), nullable = False)
     first_name = Column(String(128), nullable = True)
     last_name = Column(String(128), nullable = True)
-
-    places = relationship('place', backref = 'user', cascade = 'all, delete-orphan')
+    places = relationship('Place', backref = 'user', cascade = 'all, delete-orphan')
     reviews = relationship('Review', backref = 'user', cascade = 'all, delete-orphan')
