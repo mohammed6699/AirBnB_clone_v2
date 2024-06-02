6efd911cceb864eb7c09a31689a1b96c713c9183
mohammed6699 <135244780+mohammed6699@users.noreply.github.com>
2024-06-03 01:45:32 +0300

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,49 +4,32 @@ from flask import render_template
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+app.url_map.strict_slashes=False
 
-
-@app.route('/')
+@app.route("/")
 def hello_world():
-    """ Returns some text. """
     return 'Hello HBNB!'
 
-
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
+@app.route("/hbnb")
+def hbnb():
     return 'HBNB'
 
-
 @app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def c_is_fun(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
-
-@app.route('/python/')
+@app.route('/python', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
+def python_is_cool(text):
+    return 'Python {}'.format(text.replace('_', ' '))
 
 @app.route('/number/<int:n>')
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
+def n_is_number(n):
     return '{} is a number'.format(n)
 
-
 @app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
-    n = str(n)
-    return render_template('5-number.html', number=n)
+def html_tempelate(n):
+    return render_template("5-number.html", pagetittle="HBNB", number=n)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
