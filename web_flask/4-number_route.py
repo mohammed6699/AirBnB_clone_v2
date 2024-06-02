e991c3d68e593d88d46cb774362f55190ca9f989
mohammed6699 <135244780+mohammed6699@users.noreply.github.com>
2024-06-03 01:40:50 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+""" 4. Add fourth view function that displays var only if is integer """
+
 from flask import Flask
 
-app = Flask(__name__)
-app.url_map.strict_slashes=False
 
-@app.route("/")
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
 def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb")
-def hbnb():
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_is_fun(text):
-    return 'C {}'.format(text.replace('_', ' '))
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/')
 @app.route('/python/<text>')
-def python_is_cool(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
 @app.route('/number/<int:n>')
-def n_is_number(n):
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
     return '{} is a number'.format(n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
